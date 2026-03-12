a1e197bf1f96c251b93ea19d4dd531179b18fa54
Jay Islam <contact@jahirultusar.com>
2026-03-12 13:54:51 +0000

    Initial commit with git workflow

--- codebases/simple_server/app.py
+++ codebases/simple_server/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return "Hello, world from EC2! This is Jay from Makers :)"
 
 if __name__ == '__main__':
     app.run(
